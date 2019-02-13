b440971dc46e951a3b7109f86271f6e741b223bd
Carlos Jimenez <cjimenez@tiempodevelopment.com>
2019-02-13 16:27:02 -0600

    JS: let example update

--- JS/README.md
+++ JS/README.md
@@ -3,6 +3,23 @@
 ## let declaration
 
 The let statement declares a block scope local variable, optionally initializing it to a value.
+
+```
+{
+  let x = 1;
+
+  if (x === 1) {
+    let x = 2;
+
+    console.log(x);
+    // expected output: 2
+  }
+
+  console.log(x);
+  // expected output: 1
+}
+```
+
 Reference: [let MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let)
 
 ## const declaration
--- JS/let.js
+++ JS/let.js
@@ -4,26 +4,30 @@ as well as in any contained sub-blocks. In this way, let works very much like va
 The main difference is that the scope of a var variable is the entire enclosing function:
 */
 
-
 {
   let a = 1;
   {
     let a = 2;
     console.log(`nested scope ${a}`);
+    //expected output 2
   }
   if(true){
      let a = 3;
      console.log(`if-scope ${a}`);
+     //expected output 3
      }
   function x () {
     let a = 4
     console.log(`function-scope ${a}`);
+    //expected output 4
   }
   x();
 
   for(let a = 0; a < 1; a++){
     console.log(`for-scope ${a}`);
+    //expected output 0
   }
 
   console.log(`block-scope ${a}`);
+  //expected output 1
 }
